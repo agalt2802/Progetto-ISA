e26f4ac80e56cd601f9a42378d1b2921f5376541
Giulia Altomare <a.altomare@reply.it>
2023-04-15 19:15:15 +0200

    first draft for front-end

--- server/front-end/src/App.js
+++ server/front-end/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Container, Row} from "reactstrap";
+import Nav from "react-bootstrap/Nav";
+import'./App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import "./styles.css";
 
 function App() {
+  const handleSelectComponent = (key) => {
+    console.log(key)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container id="homePageContainer">
+      <Row>
+        <div>
+          <h1>CT INFO TRANSFER</h1>
+        </div>
+      </Row>
+      <Row>
+        <Nav justify variant="tabs" onSelect={(selectedKey) => handleSelectComponent(selectedKey)}>
+          <Nav.Item>
+            <Nav.Link eventKey={"CreateFlow"} >CREATE FLOW</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={"UpdateFlows"} >EDIT FLOW</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={"CreateCron"}  >CREATE CRON</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={"EditCron"}  >EDIT CRON</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={"ViewLogs"}  >VIEW LOGS</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Row>
+    </Container>
   );
 }
 
